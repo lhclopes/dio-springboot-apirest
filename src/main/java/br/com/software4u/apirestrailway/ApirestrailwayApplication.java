2e45c1f69b2a0e36a6e88bf31a4e29fa156e9d4b
lhclopes <luizhen@gmail.com>
2025-05-15 14:33:01 -0300

    Commit Inicial - Configuracao Springboot

---
+++ src/main/java/br/com/software4u/apirestrailway/ApirestrailwayApplication.java
@@ -0,0 +1,13 @@
+package br.com.software4u.apirestrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestrailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestrailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/software4u/apirestrailway/ApirestrailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.software4u.apirestrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestrailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
